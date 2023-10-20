cccf4c811209a023d5b0d27c28f11397117e20b2
Andreas Soete <andreas.soete@icloud.com>
2023-10-20 17:09:23 +0200

    added dotenv and fix code

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
 const result = await pool.query("SELECT * FROM users")
