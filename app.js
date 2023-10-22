1f54cb98ad0ae27ff9b48744a5ecd6e709616602
xxandreasxx <andreassoete1@gmail.com>
2023-10-22 11:54:31 +0200

    added extra functions in app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express'
 
+import { getTeam, getTeams, getUser, getUsers } from './database'
+
 const app = express()
 
-app.get("/users", (req, res) => {
-    res.send("this should be the users")
+app.get("/users", async (req, res) => {
+    const users = await getUsers()
+    res.send(users)
+})
+
+app.get("/users/:id", async (req, res) => {
+    const userId = req.params.id
+    const user = await getUser(userId)
+    res.send(user)
 })
 
 app.use((err, req, res, next) => {
@@ -13,4 +22,4 @@ app.use((err, req, res, next) => {
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080')
-})
+}) 
